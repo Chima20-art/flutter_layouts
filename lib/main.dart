1a657e8237a39499aae948698da1f1d7905fd8dc
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2023-05-03 18:11:24 +0100

    added navbar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'package:get/get.dart';
@@ -14,15 +12,37 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  SafeArea(
-      child: const  MaterialApp(
-          home:  Column(
-              children:  [
-              Flexible(child: MyProfile(),flex:1,),
-               Flexible(child: MyPosts(),flex:2, ),
+    return    SafeArea(
+      child:   MaterialApp(
+          home:  Scaffold(
+            body: const Column(
+                children:  [
+                Flexible(flex:2, child: MyProfile(),),
+                 Flexible(flex:4, child: MyPosts(), ),
+                ],
+            ),
+              bottomNavigationBar:   BottomNavigationBar(
+                backgroundColor: Colors.grey,
+            items:const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined, size: 35,),
+                 label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add_circle_outline_outlined, size: 35,),
+                 label: '',
+               
+              ),
+                  BottomNavigationBarItem(
+                icon: Icon(Icons.save_alt_rounded, size: 35,),
+                 label: '',
+               
+              ),
+              // add your third icon here
+            ],
+           
+          ),
             
-                
-              ],
           ),
         ),
       
@@ -100,12 +120,12 @@ class ProfileInfo extends StatelessWidget {
               children: [
                 Icon(
                  Icons.arrow_back, 
-                 size: 25,
+                 size: 27,
                  color: Colors.white,
                  ),
                 Icon(
                   Icons.settings,
-                  size: 25,
+                  size: 27,
                   color: Colors.white,
                 ),
               ],
@@ -122,7 +142,7 @@ class ProfileInfo extends StatelessWidget {
                   backgroundImage: AssetImage("assets/profilepic.jpg"),
                 ),
                 SizedBox(height: 10,),
-                Text('John Blayne', style: TextStyle(color:Colors.white, fontSize: 16) ,),
+                Text("John Blayne", style: TextStyle(color:Colors.white, fontSize: 16)),
                 SizedBox(height: 10,),
 
                 Row(
@@ -133,7 +153,7 @@ class ProfileInfo extends StatelessWidget {
                      children: [
                         Icon(Icons.people, size: 16,color: Colors.grey),
                          SizedBox(width: 5,),
-                        Text("600 Followers", style: TextStyle(color:Colors.white, fontSize: 12) ,),
+                        Text("600 Followers", style: TextStyle(color:Colors.white, fontSize: 12)),
                       
                       ],
                     
@@ -175,40 +195,67 @@ class MyPosts extends StatefulWidget {
   State<MyPosts> createState() => _MyPostsState();
 }
 
+class AppPost{
+
+}
+
 class _MyPostsState extends State<MyPosts> {
 
-  final List<String> _listItem = [
-    'assets/one.jpg',
-    'assets/two.jpg',
-    'assets/three.jpg',
-    'assets/four.jpg',
-    'assets/five.jpg',
-    'assets/six.jpg',
-    'assets/one.jpg',
-    'assets/two.jpg',
-  ];
+  final List<Map<String, String>> _listItem = [
+   { 'image':'assets/one.jpg','views':'1.5k','likes':'229'},
+    { 'image':'assets/two.jpg','views':'2.5k','likes':'259'},
+     { 'image':'assets/three.jpg','views':'14.5k','likes':'229'},
+      { 'image':'assets/four.jpg','views':'13.5k','likes':'259'},
+       { 'image':'assets/five.jpg','views':'18.5k','likes':'259'},
+        { 'image':'assets/six.jpg','views':'11.5k','likes':'229'},
+         { 'image':'assets/one.jpg','views':'10.5k','likes':'229'},
+          { 'image':'assets/two.jpg','views':'15.5k','likes':'259'},
+  ] ;
 
   @override
   Widget build(BuildContext context) {
+    debugPrint(_listItem.toString());
     return   Container(
             height: double.infinity,
-            color: Colors.grey.shade900,
+            color: Colors.black,
             child: GridView.count(
               crossAxisCount: 2,
-              padding: const EdgeInsets.symmetric(vertical:30, horizontal: 35),
+              padding: const EdgeInsets.symmetric(vertical:30, horizontal: 30),
               crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
+              mainAxisSpacing: 15,
+              childAspectRatio: (1 / 1.17),
               children: _listItem.map((item) => Card(
-                color:Colors.transparent,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    image: DecorationImage(
-                      image: AssetImage(item),
-                      fit:BoxFit.cover
+              color:  Colors.transparent,
+                child:  Column(
+                  children: [
+                    Expanded(
+                    child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.asset(item['image'] ?? "",fit: BoxFit.cover, height: 100.0,),
                     ),
-                  ) ,
-                ) ,
+                ),
+                  const SizedBox(height: 10,),
+                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Text(item['views']!,style: TextStyle(color: Colors.white),),
+                          const SizedBox(width: 5,),
+                          const  Icon(Icons.remove_red_eye, color: Colors.white, size: 13,)
+                        ],
+                      ),
+                        Row(
+                        children: [
+                          Text(item['likes']!,style: TextStyle(color: Colors.white),),
+                          const SizedBox(width: 5,),
+                          const Icon(Icons.heart_broken_rounded, color: Colors.white, size: 13,)
+                        ],
+                      ),
+                    ],
+                  ),
+                  ],
+                ),
               )).toList(),
               ),
               
@@ -216,3 +263,77 @@ class _MyPostsState extends State<MyPosts> {
       
   }
 }
+
+
+// class _MyPostsState extends State<MyPosts> {
+
+//   final Future<List<dynamic>?> _listItem = [
+//    { 'image':'assets/one.jpg','views':'1.5k','likes':'229'},
+//     { 'image':'assets/two.jpg','views':'1.5k','likes':'229'},
+//      { 'image':'assets/three.jpg','views':'1.5k','likes':'229'},
+//       { 'image':'assets/four.jpg','views':'1.5k','likes':'229'},
+//        { 'image':'assets/five.jpg','views':'1.5k','likes':'229'},
+//         { 'image':'assets/six.jpg','views':'1.5k','likes':'229'},
+//          { 'image':'assets/one.jpg','views':'1.5k','likes':'229'},
+//           { 'image':'assets/two.jpg','views':'1.5k','likes':'229'},
+//   ] 
+
+//   @override
+//   Widget build(BuildContext context) {
+//     debugPrint(_listItem.toString());
+//     return   Container(
+//             height: double.infinity,
+//             color: Colors.black,
+//             child: GridView.count(
+//               crossAxisCount: 2,
+//               padding: const EdgeInsets.symmetric(vertical:30, horizontal: 30),
+//               crossAxisSpacing: 10,
+//               mainAxisSpacing: 15,
+//               childAspectRatio: (1 / 1.17),
+//               children: <Widget>[
+//                  FutureBuilder(
+//                   future: _listItem,
+//                   builder: (context, snapshot) {
+//                     return
+//                             Card(
+//                 color:  Colors.transparent,
+//                 child:  Column(
+//                   children: [
+//                     Expanded(
+//                     child: ClipRRect(
+//                     borderRadius: BorderRadius.circular(15),
+//                     child: Image.asset('assets/one.jpg',fit: BoxFit.cover, height: 100.0,),
+//                     ),
+//                 ),
+//                   const SizedBox(height: 10,),
+//                   const Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                     children: [
+//                       Row(
+//                         children: [
+//                           Text("1.5K",style: TextStyle(color: Colors.white),),
+//                           SizedBox(width: 5,),
+//                           Icon(Icons.remove_red_eye, color: Colors.white, size: 13,)
+//                         ],
+//                       ),
+//                         Row(
+//                         children: [
+//                           Text("229 likes",style: TextStyle(color: Colors.white),),
+//                           SizedBox(width: 5,),
+//                           Icon(Icons.heart_broken_rounded, color: Colors.white, size: 13,)
+//                         ],
+//                       ),
+//                     ],
+//                   ),
+//                   ],
+//                 ),
+//               );
+//                   }
+//                  )
+//                  ]
+// //               ),
+              
+// //               );
+      
+// //   }
+// // }
